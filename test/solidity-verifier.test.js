0a813fd8701a6bc7d7debb754880769398699f62
tungpham <tungpham@lecle.co.kr>
2023-11-15 14:15:31 +0700

    add over 21 circuit

--- test/solidity-verifier.test.js
+++ test/solidity-verifier.test.js
@@ -6,6 +6,7 @@ const HASH_CIRCUIT_PATH_SUFFIX = "hash/";
 const POLYNOM_CIRCUIT_PATH_SUFFIX = "simple-polynomial/";
 const NONZERO_CIRCUIT_PATH_SUFFIX = "none-zero/";
 const AVERAGE_CIRCUIT_PATH_SUFFIX = "average/";
+const OVER21_CIRCUIT_PATH_SUFFIX = "over21/";
 
 describe("Testing zk proof generation and solidity verifier", () => {
 
@@ -76,7 +77,7 @@ describe("Testing zk proof generation and solidity verifier", () => {
     expect(tx).to.equal(true);
   });
 
-  it.only("Should deploy Average verifier and execute on-chain zk verification successfully", async () => {
+  it("Should deploy Average verifier and execute on-chain zk verification successfully", async () => {
     // deploy contract
     const Verifier = await ethers.getContractFactory("contracts/AverageVerifier.sol:Verifier");
     const verifier = await Verifier.deploy();
@@ -86,6 +87,27 @@ describe("Testing zk proof generation and solidity verifier", () => {
 
     // generate proof call data
     const {pi_a, pi_b, pi_c, input} = await generateCallData(AVERAGE_CIRCUIT_PATH_SUFFIX);
+    console.log(">>>proof and expected value: ", {pi_a, pi_b, pi_c, input})
+
+    // verify proof on contract
+    //@ts-ignore
+    const tx = await verifier.verifyProof(pi_a, pi_b, pi_c, input)
+    
+    console.log(`Verifier result: ${tx}`)
+    // console.assert(tx == true, "Proof verification failed!");
+    expect(tx).to.equal(true);
+  });
+
+  it("Should deploy over21 verifier and execute on-chain zk verification successfully", async () => {
+    // deploy contract
+    const Verifier = await ethers.getContractFactory("contracts/Over21Verifier.sol:Verifier");
+    const verifier = await Verifier.deploy();
+    await verifier.deployed();
+
+    console.log(`Verifier deployed to ${verifier.address}`);
+
+    // generate proof call data
+    const {pi_a, pi_b, pi_c, input} = await generateCallData(OVER21_CIRCUIT_PATH_SUFFIX);
     console.log(">>>proof and expected value: ", {pi_a, pi_b, pi_c, input})
 
     // verify proof on contract
